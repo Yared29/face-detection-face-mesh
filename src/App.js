c916740ddb208da43e1c5759be78913390ea48dd
unknown <yaredtekile5@gmail.com>
2021-07-15 09:07:05 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,91 @@
-import logo from './logo.svg';
+import React, { useRef, useEffect } from 'react';
+import * as tf from '@tensorflow/tfjs';
+import Webcam from 'react-webcam';
+import * as facemesh from '@tensorflow-models/face-landmarks-detection';
+import { drawMesh } from './utilities';
 import './App.css';
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  const runFacemesh = async () => {
+    const net = await facemesh.load(
+      facemesh.SupportedPackages.mediapipeFacemesh
+    );
+    setInterval(() => {
+      detect(net);
+    }, 10);
+  };
+
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== 'undefined' &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // Make Detections
+      const face = await net.estimateFaces({ input: video });
+      console.log(face);
+
+      // Get canvas context
+      const ctx = canvasRef.current.getContext('2d');
+
+      requestAnimationFrame(() => {
+        drawMesh(face, ctx);
+      });
+    }
+  };
+
+  useEffect(() => {
+    runFacemesh();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <Webcam
+          ref={webcamRef}
+          mirrored={true}
+          style={{
+            position: 'absolute',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            left: 0,
+            right: 0,
+            textAlign: 'center',
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: 'absolute',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            left: 0,
+            right: 0,
+            textAlign: 'center',
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
       </header>
     </div>
   );
